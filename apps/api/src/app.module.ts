5e2084332b6bda4b11112477f9423b4276240626
Roland Chelwing <jolleee@hotmail.com>
2026-01-07 12:42:42 +0100

    chore: fix env for api app

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { HealthController } from './health.controller'
 import { TrpcModule } from './trpc/trpc.module'
 
 @Module({
-	imports: [TrpcModule],
+	imports: [ConfigModule.forRoot(), TrpcModule],
 	controllers: [HealthController],
 })
 export class AppModule {}
